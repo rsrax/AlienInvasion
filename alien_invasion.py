a70060d6bd44e3ae01c7cb06dd5b40aa719f045f
rsrax <rs.wolviepool1499@gmail.com>
2020-07-19 13:56:24 +0530

    Added functionality to quit with 'q' press

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,6 +38,8 @@ class AlienInvasion:
             self.ship.move_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.move_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """ 
