505c0a649111c680e98a1adb50a13dac194ea266
rsrax <rs.wolviepool1499@gmail.com>
2020-07-19 13:46:28 +0530

    Refactored event checking code

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,15 +26,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.move_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.move_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.move_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.move_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """ 
+        * Respond to keypresses. *
+        """
+        if event.key == pygame.K_RIGHT:
+            self.ship.move_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.move_left = True
+
+    def _check_keyup_events(self, event):
+        """ 
+        * Respond to key releases. *
+        """
+        if event.key == pygame.K_RIGHT:
+            self.ship.move_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.move_left = False
 
     def _update_screen(self):
         # Redraw the screen after each pass.
