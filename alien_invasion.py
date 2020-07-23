641456249927474fe048c1570edf1da8c12af060
rsrax <rs.wolviepool1499@gmail.com>
2020-07-24 00:54:11 +0530

    Refactored Bullet Code

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,17 +33,29 @@ class AlienInvasion:
         """ Create the fleet of aliens """
         # Create an alien and find the number of aliens in a row
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2*alien_width)
         number_aliens_x = available_space_x // (3*alien_width)
 
-        # Create the first row of aliens.
-        for alien_number in range(number_aliens_x):
-            # Create an alien and place it in the row
-            alien = Alien(self)
-            alien.x = 3 * alien_width + 3 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+        # Determine the number of rows of aliens that fit on the screen.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3*alien_height)-3*ship_height)
+        number_rows = available_space_y // (3*alien_height)
+
+        # Create the full fleet of aliens.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        # Create an alien and place it in the row
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = 3 * alien_width + 3 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 3*alien.rect.height * row_number
+        self.aliens.add(alien)
 
     def _check_events(self):
         # Watch for keyboard and mouse events.
@@ -87,6 +99,10 @@ class AlienInvasion:
         # Refresh the screen frames, update display
         pygame.display.flip()
 
+    def _update_aliens(self):
+        """ Update the position of all aliens in the fleet. """
+        self.aliens.update()
+
     def _fire_bullet(self):
         """ Create a new bullet and add it to the bullets group """
         if len(self.bullets) < self.settings.bullets_allowed:
@@ -111,6 +127,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
